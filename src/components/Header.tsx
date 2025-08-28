cff74836edcbc93e05c3c1df3ee82f22897c25eb
Vitor Hugo <vitor4ntunes@gmail.com>
2025-08-28 18:16:53 -0300

    feat: add real github link to header and cursor pointer to search div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Github, Menu, Search } from 'lucide-react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { SearchCommandKey } from './Search/SearchCommandKey';
-import SearchModal from './SearchModal';
+import SearchModal from './Search/SearchModal';
 
 export default function Header() {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -26,7 +26,6 @@ export default function Header() {
     <>
       <header className="sticky top-0 z-40 w-full border-b border-gray-300 bg-white/70 backdrop-blur-md supports-[backdrop-filter]:bg-white/60 ">
         <div className="flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8 max-w-[1440px] mx-auto">
-          {/* Left section - Logo and Navigation */}
           <div className="flex items-center gap-24">
             <div className="flex items-center gap-4">
               <button
@@ -49,7 +48,7 @@ export default function Header() {
                 onClick={() => setIsSearchOpen(true)}
                 className="relative group"
               >
-                <div className="flex items-center w-96 rounded-lg border border-gray-300 bg-gray-50 py-2 px-3 text-sm text-left transition-all group-hover:bg-gray-100 group-hover:border-gray-400">
+                <div className="flex items-center w-96 rounded-lg border border-gray-300 bg-gray-50 py-2 px-3 text-sm text-left transition-all group-hover:bg-gray-100 group-hover:border-gray-400 cursor-pointer">
                   <Search className="h-4 w-4 text-gray-400 mr-3 flex-shrink-0" />
                   <span className="text-gray-500 flex-1 text-left truncate">
                     Buscar na documentação...
@@ -69,7 +68,7 @@ export default function Header() {
               </button>
 
               <Link
-                href="https://github.com"
+                href="https://github.com/VitorHugoAntunes/Docs"
                 className="p-2 text-gray-500 hover:text-gray-700 -me-2.5"
                 target="_blank"
                 rel="noopener noreferrer"
