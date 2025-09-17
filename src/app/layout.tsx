8f0008d73cd4d7bbc8b73cad7fb68689a86577ab
Vitor Hugo <vitor4ntunes@gmail.com>
2025-09-17 01:34:59 -0300

    style: change font style from geist to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,11 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { SidebarProvider } from "@/contexts/SidebarContext";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -30,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <SidebarProvider>
           <Header />
